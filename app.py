aa9b1e354987fee697f58a4138e47a2285f4dd4e
THASNEEM VELLARAMPARA <60733459+THASNEEMMOOOSA@users.noreply.github.com>
2024-03-01 14:07:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask,render_template,request,redirect,url_for
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def landing():
+    return render_template('landing.html')
+
+
+
+@app.route('/hello')
+def helloworld():
+    return render_template('hello.html')
+
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    err=None
+    if request.method =='POST':
+        if request.form['uname']!='john' or  request.form['pswrd']!='12345':
+            err='Invalid username and password!!!'
+        else:
+            return redirect(url_for('home'))
+
+    return render_template('login.html',err=err)
+
+if __name__=='__main__':
+    app.run()
